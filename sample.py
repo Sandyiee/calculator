938a92b5abeb7564ec0f953fba552551fa74748c
Sandyiee <hsohtnas3@gmail.com>
2025-06-11 12:27:40 +0530

    add subtract operator

--- sample.py
+++ sample.py
@@ -1,11 +1,14 @@
 def add(a,b):
     return a+b
+def sub(a,b):
+    return a-b
 
 def main():
     print("welcome to calc!")
     a=int(input("Enter the first number :"))
     b=int(input("Enter the second number :"))
     print(f"add :{add(a,b)}")
+    print(f"sub :{sub(a,b)}")
 
 if __name__ =="__main__":
     main()
