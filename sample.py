08a56fec9e4d9149cde371e9fed1f3925baae10b
Sandyiee <hsohtnas3@gmail.com>
2025-06-11 12:22:45 +0530

    add adition operator

--- sample.py
+++ sample.py
@@ -1,4 +1,11 @@
+def add(a,b):
+    return a+b
+
 def main():
     print("welcome to calc!")
+    a=int(input("Enter the first number :"))
+    b=int(input("Enter the second number :"))
+    print(f"add :{add(a,b)}")
+
 if __name__ =="__main__":
     main()
