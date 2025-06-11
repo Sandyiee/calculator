c81b853ba153b6c4b290153bd22cae93c10ae49f
Sandyiee <hsohtnas3@gmail.com>
2025-06-11 12:29:45 +0530

    add multiplication  operator

--- sample.py
+++ sample.py
@@ -2,6 +2,8 @@ def add(a,b):
     return a+b
 def sub(a,b):
     return a-b
+def mul(a,b):
+    return a*b
 
 def main():
     print("welcome to calc!")
@@ -9,6 +11,7 @@ def main():
     b=int(input("Enter the second number :"))
     print(f"add :{add(a,b)}")
     print(f"sub :{sub(a,b)}")
+    print(f"mul :{mul(a,b)}")
 
 if __name__ =="__main__":
     main()
